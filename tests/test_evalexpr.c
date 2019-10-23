12be1a69d986e35bbcc0ff677dc9cf1fa838e19a
Thomas Jouanolle <tjouanolle@localhost.localdomain>
2019-10-23 14:34:53 +0200

    Add updated test_parseek.c

--- tests/test_evalexpr.c
+++ tests/test_evalexpr.c
@@ -10,7 +10,7 @@
 
 char *parenthesis_seeker(char *str);
 
-Test(evalexpr, parentesis_seeker)
+Test(parseek, parentesis_seeker)
 {
     char str[23] = "((2*3+5)*(10*(20-2)/5))";
     char *res = parenthesis_seeker(str);
@@ -18,7 +18,7 @@ Test(evalexpr, parentesis_seeker)
     cr_assert_str_eq(res, "..330....................");
 }
 
-Test(evalexpr, parentesis_seeker2)
+Test(parseek, parentesis_seeker2)
 {
     char str[23] = "((3+2)*5)";
     char *res = parenthesis_seeker(str);
@@ -26,7 +26,7 @@ Test(evalexpr, parentesis_seeker2)
     cr_assert_str_eq(res, "..25.......");
 }
 
-Test(evalexpr, parentesis_seeker3)
+Test(parseek, parentesis_seeker3)
 {
     char str[23] = "(2*3+5)*(10*(20-2)/5)";
     char *res = parenthesis_seeker(str);
@@ -34,8 +34,7 @@ Test(evalexpr, parentesis_seeker3)
     cr_assert_str_eq(res, ".330...................");
 }
 
-
-Test(evalexpr, parentesis_seeker4)
+Test(parseek, parentesis_seeker4)
 {
     char str[23] = "(3+2)*5";
     char *res = parenthesis_seeker(str);
