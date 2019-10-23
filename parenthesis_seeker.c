4452d088e4afd50399bfce2ecb0742d3d6ce58bc
Thomas Jouanolle <tjouanolle@localhost.localdomain>
2019-10-23 13:56:07 +0200

    Add updated parenthesis_seeker.c and new tests

--- parenthesis_seeker.c
+++ parenthesis_seeker.c
@@ -7,8 +7,23 @@
 
 #include "my.h"
 #include <stdlib.h>
+#include <stdio.h>
+
+char *compute(char *operation);
 
 char *replace(char *str, char *seg, int begin, int seg_len)
+{
+    for (int i = 0; i < seg_len; i++) {
+        if (i < my_strlen(seg)) {
+            str[begin + i] = seg[i];
+        } else {
+            str[begin + i] = '.';
+        }
+    }
+    return str;
+}
+
+char *replace2(char *str, char *seg, int begin, int seg_len)
 {
     for (int i = 0; i < seg_len; i++) {
         if (i < my_strlen(seg) && i != 0 && i != seg_len - 1) {
@@ -30,15 +45,13 @@ int operation_lenght(char *ops, char operator)
         if (ops[op_len] == operator)
             op++;
         op_len++;
-        if ((ops[op_len] < '9' && ops[op_len] > '0') || ops[op_len] == '.') {
+        if ((ops[op_len] <= '9' && ops[op_len] >= '0') || ops[op_len] == '.') {
             nb_or_point = 1;
         } else
             nb_or_point = 0;
     }
     return op_len;
 }
-
-char *compute(char *operation);
 
 char *operator_seeker(char *seg, int seg_lenght, int operator)
 {
@@ -54,23 +67,44 @@ char *operator_seeker(char *seg, int seg_lenght, int operator)
             operation = my_strncpy(operation, &seg[begin], op_lenght);
             operation = compute(operation);
             seg = replace(seg, operation, begin, op_lenght);
+            free(operation);
             operation = NULL;
-        } else if (seg[i] > '9' || seg[i] < '0') {
+            i = 0;
+            begin = 0;
+        } else if ((seg[i] > '9' || seg[i] < '0') && seg[i] != '.')
             begin = i + 1;
-        }
     }
     if (operator < 5)
-        operator_seeker(seg, seg_lenght, operator + 1);
-    free(operation);
+        seg = operator_seeker(seg, seg_lenght, operator + 1);
     return seg;
+}
+
+void add_parenthesis(char *str)
+{
+    char *temp = malloc(sizeof(char) * my_strlen(str) + 2);
+
+    temp[0] = '(';
+    temp[my_strlen(str) + 1] = ')';
+    temp[my_strlen(str) + 2] = '\0';
+    my_strncpy(&temp[1], str, my_strlen(str));
+    printf("STR BEFORE : %s\n", str);
+    printf("TEMP BEFORE : %s\n", temp);
+    str = temp;
+    printf("TEMP AFTER : %s\n", temp);
+    printf("STR AFTER : %s\n", str);
+    //free(temp);
+    printf("STR : %s\n", str);
 }
 
 int parenthesis_seeker(char *str)
 {
+    //printf("STR BEFORE : %s\n", str);
     int open_par = 0;
     char *seg;
     int seg_lenght = 0;
 
+    add_parenthesis(str);
+    //printf("STR MIDDLE : %s\n", str);
     for (int i = 0; i < my_strlen(str); i++) {
         if (str[i] == '(') {
             open_par = i;
@@ -79,11 +113,12 @@ int parenthesis_seeker(char *str)
             seg = malloc(seg_lenght);
             seg = my_strncpy(seg, &str[open_par], seg_lenght);
             seg = operator_seeker(seg, seg_lenght, 0);
-            str = replace(str, seg, open_par, seg_lenght);
+            str = replace2(str, seg, open_par, seg_lenght);
             seg = NULL;
             i = -1;
         }
     }
     free(seg);
-    return 0;
+    //printf("STR AFTER : %s\n", str);
+    return 1;
 }
--- tests/test_evalexpr.c
+++ tests/test_evalexpr.c
@@ -12,16 +12,37 @@ int parenthesis_seeker(char *str);
 
 Test(evalexpr, parentesis_seeker)
 {
-    char str[23] = "(2*3+5)*(10*(20-2)/5)";
+    char str[23] = "((2*3+5)*(10*(20-2)/5))";
     int ret = parenthesis_seeker(str);
 
     printf("Res: %s \n", str);
-    cr_assert_str_eq(str, "396..................");
+    cr_assert_str_eq(str, ".330...................");
     cr_assert(ret == 1);
 }
 
 
 Test(evalexpr, parentesis_seeker2)
+{
+    char str[23] = "((3+2)*5)";
+    int ret = parenthesis_seeker(str);
+
+    printf("Res: %s \n", str);
+    cr_assert_str_eq(str, ".25......");
+    cr_assert(ret == 1);
+}
+
+Test(evalexpr, parentesis_seeker3)
+{
+    char str[23] = "(2*3+5)*(10*(20-2)/5)";
+    int ret = parenthesis_seeker(str);
+
+    printf("Res: %s \n", str);
+    cr_assert_str_eq(str, "330..................");
+    cr_assert(ret == 1);
+}
+
+
+Test(evalexpr, parentesis_seeker4)
 {
     char str[23] = "(3+2)*5";
     int ret = parenthesis_seeker(str);
